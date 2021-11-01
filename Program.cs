b06f952abbaab24ae2c09f8b54723c5781829c83
TIANGeng708 <11810417@mail.sustech.edu.cn>
2021-11-01 19:25:30 +0800

    Back end1

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            try
+            {
+                using (StreamReader sr = new StreamReader("D:/Project_of_c#/ConsoleApp2/ConsoleApp2/test.txt"))
+                {
+                    string line;
+                     
+                    while ((line = sr.ReadLine()) != null)
+                    {   
+                        Console.WriteLine(count.ToString()+' '+line);
+                        // Console.WriteLine(count+' '+line);
+                        count++;
+                        // Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
