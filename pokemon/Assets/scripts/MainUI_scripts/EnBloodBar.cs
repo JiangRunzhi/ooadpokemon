c535d4cb2f877f3369e7e66ca4077086209017e0
Tankkupper <11910117@mail.sustech.edu.cn>
2021-11-16 21:54:05 +0800

    血条

---
+++ pokemon/Assets/scripts/MainUI_scripts/EnBloodBar.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class EnBloodBar : MonoBehaviour
+{
+    private float curBlood = 0f;
+    private float targetBlood = 1.0f;
+    public Slider bloodBar;
+
+    public int damage = 1;
+    private int hpHolder = 2000;
+    private int hp;
+
+    private void Start()
+    {
+        hp = hpHolder;
+        bloodBar.value = 1000;
+        //初始化
+        //绑定按钮点击事件
+        //buttonAdd->addBlood
+        //buttonDes->desBlood
+    }
+
+    public void addBlood()
+    {
+        //加血
+    }
+
+    public void desBlood()
+    {
+        //减血 
+    }
+    
+    void TakeDamge()//当受到伤害
+    {
+        if(hp<0) return;
+        hp-=damage;
+        bloodBar.value = (float)hp/hpHolder;//通过改变value的值（float类型）来改变血条长度。
+        if(hp<=0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void Update()
+    {
+        //更新血量
+
+        TakeDamge();
+
+        curBlood = (float) hp / hpHolder;
+
+        bloodBar.value = curBlood;
+    }
+}
