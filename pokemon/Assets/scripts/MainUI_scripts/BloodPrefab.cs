3210f636ffb3934875c1837863fbf3381da69e12
Tankkupper <11910117@mail.sustech.edu.cn>
2021-11-18 21:56:21 +0800

    bloodroll renewgit add .git add .git add .git add .git add .git add .git add .!

---
+++ pokemon/Assets/scripts/MainUI_scripts/BloodPrefab.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class BloodPrefab : MonoBehaviour
+{
+    public GameObject myBloodPrefab;
+    public GameObject enBloodPrefab;
+    public bool newBloodRoll;
+
+    void Start()
+    {
+        //newBloodRoll = false;
+    }
+
+    void Update()
+    {
+        if (newBloodRoll)
+        {
+            NewObject();
+            newBloodRoll = false;
+        }
+    }
+
+    void NewObject()
+    {
+        GameObject myBlood = Instantiate(myBloodPrefab);
+        GameObject enBlood = Instantiate(enBloodPrefab);
+        myBlood.transform.SetParent(transform, false);
+        enBlood.transform.SetParent(transform, false);
+    }
+}
