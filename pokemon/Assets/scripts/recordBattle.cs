e11d9b5989cc6c23be5420205191c470892951f7
TIANGeng708 <11810417@mail.sustech.edu.cn>
2021-11-21 19:47:56 +0800

    核心控制脚本加入

---
+++ pokemon/Assets/scripts/recordBattle.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class recordBattle : MonoBehaviour
+{
+    public static int bloodLeft = 0;
+
+    public static int defenseLeft = 0;
+
+    public static int attackLeft = 0;
+
+    public static int speedLeft = 0;
+    
+    public static int bloodRight = 0;
+
+    public static int defenseRight = 0;
+
+    public static int attackRight = 0;
+
+    public static int speedRight = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ pokemon/Assets/scripts/recordProperty.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class recordProperty : MonoBehaviour
+{
+    //这是妙蛙种子
+    public static int bloodBulbasaur = 15;//2=5*3
+
+    public static int defenseBulbasaur = 3;//1*3
+
+    public static int attackBulbasaur = 6;//2*3
+
+    public static int speedBulbasaur = 3;//1*3
+    
+    //这是小火龙
+    public static int bloodCharmander = 15;//2=5*3
+
+    public static int defenseCharmander = 3;//1*3
+
+    public static int attackCharmander = 8;//2*4
+
+    public static int speedCharmander = 4;//1*4
+
+    //这是杰尼龟
+    public static int bloodSquirtle = 15;//2=5*3
+
+    public static int defenseSquirtle = 4;//1*4
+
+    public static int attackSquirtle = 6;//2*3
+
+    public static int speedSquirtle = 3;//1*3
+    
+    //这是比比鸟
+    public static int bloodPidgeotto = 15;//2=5*3
+
+    public static int defensePidgeotto = 3;//1*3
+
+    public static int attackPidgeotto = 6;//2*3
+
+    public static int speedPidgeotto = 4;//1*4
+    
+    //这是皮卡丘
+    public static int bloodPikachu = 15;//2=5*3
+
+    public static int defensePikachu = 3;//1*3
+
+    public static int attackPikachu = 8;//2*4
+
+    public static int speedPikachu = 6;//1*6
+    
+    //这是臭臭泥
+    public static int bloodMuk = 20;//5*4
+
+    public static int defenseMuk = 3;//1*3
+
+    public static int attackMuk = 10;//2*5
+
+    public static int speedMuk = 3;//1*3
+
+
+
+
+
+
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
